98fc023ca33aa84624bd6811ee078dce720df82d
gidi2345 <gidon1smila@gmail.com>
2021-03-04 11:40:13 +0200

    mm

--- my-app/src/pages/splash-page/SplashPage.tsx
+++
@@ -1,9 +0,0 @@
-import './SplashPage.css';
-import LottieControl from "../../animations/LoveSlotMachine";
-export const Welcome = (props: any) => {
-
-
-    return <div className="cstyle"   ><LottieControl></LottieControl><div></div></div>;
-}
-
-
